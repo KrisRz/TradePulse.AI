e92bb8bd19447d1116bcac3af37b2c47da9ebbe5
Krzysztof Rzepka <krisrz@mac.home>
2025-09-29 18:29:36 +0100

    Route53: set allow_overwrite=true for backend CNAME to UPSERT existing record

--- infra/main.tf
+++ infra/main.tf
@@ -306,6 +306,7 @@ resource "aws_route53_record" "backend_alias" {
   name    = local.backend_fqdn
   type    = "CNAME"
   ttl     = 60
+  allow_overwrite = true
   records = [aws_apprunner_service.backend.service_url]
 }
 
